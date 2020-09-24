bdb0621cfffc63705bb7928a1bd52c1f75e99e50
alex_ <alex_@LAPTOP-8FE7M6V1>
2020-09-24 19:41:25 +0200

    up4

--- proyecto_gib/src/proyecto_gib/clase1_.java
+++ proyecto_gib/src/proyecto_gib/clase1_.java
@@ -5,13 +5,12 @@ public class clase1_ {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		int num=2;
-		
-		String nombre="prueba_git";
-		//edicion git hub
+		int num = 2;
+
+		String nombre = "prueba_git";
+		// edicion git hub
 		// cambio desde eclipse
-		
+
 	}
 
 }
